56a701fdc3fe4a4c5bb3b9a958ce32e43fae1b3b
Omid <omidbros3@gmail.com>
2024-04-01 22:16:10 +0330

    hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,6 +8,10 @@ const Hero = () => {
                 <p className='text-[20px]'>UX Designer</p>
                 <p className='text-[48px] font-bold max-lg:text-[2rem]'>Hi There, I’m<br /><span className='text-[#FC3314]'>Sarah Jones</span></p>
                 <p className='max-w-[457px] max-lg:text-center'>Welcome to my portfolio of captivating digital experiences. Explore my work and let's create something extraordinary together.</p>
+                <div className='flex gap-5 mt-5'>
+                    <button className='text-white w-[177px] h-12 bg-black' type='button'>Hire Me</button>
+                    <button className='text-black border-solid border-2 border-zinc-950 w-[177px] h-12 bg-white' type='button'>Portfolio</button>
+                </div>
             </div>
             <div>
                 <img src={hero} alt="hero" />
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,11 +7,13 @@ const Sidebar = () => {
 
     return (
         <>
-            <div className={`absolute z-20 top-0 left-0 w-[260px] h-full bg-green-500 transition-all ease-in-out duration-[0.3s] ${isTrue ? 'translate-x-[-260px]' : 'translate-x-0'}`}></div>
-            <div
-                className={`absolute inset-0 z-10 bg-[#00000065] transition-all ease-in-out duration-[0.3s] ${isTrue ? 'invisible opacity-0' : 'visible opacity-100'}`}
-                onClick={() => setIsTrue(!isTrue)}
-            ></div>
+            <div className='hidden max-lg:block'>
+                <div className={`absolute z-20 top-0 left-0 w-[260px] h-full bg-green-500 transition-all ease-in-out duration-[0.3s] ${isTrue ? 'translate-x-0' : 'translate-x-[-260px]'}`}></div>
+                <div
+                    className={`absolute inset-0 z-10 bg-[#00000065] transition-all ease-in-out duration-[0.3s] ${isTrue ? 'visible opacity-100' : 'invisible opacity-0'}`}
+                    onClick={() => setIsTrue(!isTrue)}
+                ></div>
+            </div>
         </>
     )
 }
